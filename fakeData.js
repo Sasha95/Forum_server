cdcd0981195f9c47bea23fe8994e2eef4efdacc2
User <sashamangir@mail.ru>
2017-10-16 14:38:28 +0300

    фэйк база

---
+++ fakeData.js
@@ -0,0 +1,65 @@
+const fs = require('fs')
+const faker = require('faker')
+const json2csv = require('json2csv')
+
+faker.locale = 'ru'
+
+const getPerson = id => {
+  return { id, name: faker.name.findName() }
+}
+
+const getPost = (id, person) => {
+  const textPreview = faker.lorem.paragraphs()
+  return {
+    id,
+    person_id: person.id,
+    person_name: person.name,
+    title: faker.lorem.sentences(),
+    post:
+      textPreview +
+      faker.lorem.paragraphs() +
+      faker.lorem.paragraphs() +
+      faker.lorem.paragraphs() +
+      faker.lorem.paragraphs(),
+    textPreview,
+    created_at: faker.date.recent()
+  }
+}
+
+const getRandomInt = max => {
+  return Math.floor(Math.random() * max) + 1
+}
+
+const numberOfPerson = 20
+const maxfPosts = 15
+const persons = []
+const posts = []
+
+let postIndex = 1
+
+for (let i = 1; i <= numberOfPerson; i++) {
+  const numberOfPosts = getRandomInt(maxfPosts)
+  let person = getPerson(i)
+  persons.push(person)
+  for (let j = 1; j <= numberOfPosts; j++) {
+    let post = getPost(postIndex, person)
+    posts.push(post)
+
+    postIndex++
+  }
+}
+const db = { persons, posts }
+fs.writeFile('./data/db.json', JSON.stringify(db, null, 2), er => {
+  if (er) throw er
+  console.log('succsessfully')
+})
+
+fs.writeFile('./data/persons.csv', json2csv({ data: persons }), er => {
+  if (er) throw er
+  console.log('succsessfully')
+})
+
+fs.writeFile('./data/posts.csv', json2csv({ data: posts }), er => {
+  if (er) throw er
+  console.log('succsessfully')
+})
